30f3395c731dfb83d7fc8ae0c9fd4b8731e9cbde
Bowen Zhang <zbw14@outlook.com>
2022-06-09 18:23:21 +0000

    Update evaluation tools and update readme

--- tools/run_associate.sh
+++ tools/run_associate.sh
@@ -1,1 +1,2 @@
-python3 associate.py PATH_TO_SEQUENCE/rgb.txt PATH_TO_SEQUENCE/depth.txt > PATH_TO_SEQUENCE/associate.txt
+data_folder="/home/azureuser/dataset/rgbd_dataset_freiburg1_room"
+python3 tools/associate.py $data_folder/rgb.txt $data_folder/depth.txt > $data_folder/associate.txt
